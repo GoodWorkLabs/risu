1b4a9b79bed6bc527515480de03216a253447fb0
Ujjal Bhattacharya <ujjal.bhattacharya@goodworklabs.com>
2016-05-10 14:18:44 +0530

    Fixed file parsing issue

--- lib/risu/parsers/nessus/nessus_sax_listener.rb
+++ lib/risu/parsers/nessus/nessus_sax_listener.rb
@@ -65,13 +65,13 @@ module Risu
 
 				# An array of all valid elements expected during parsing
 				VALID_ELEMENTS = VALID_REFERENCES \
-					+ Set.new(%w[NessusReportItem plugin_version risk_factor
+					+ Set.new(%w[ReportItem plugin_version risk_factor
 					description cvss_base_score solution item plugin_output tag synopsis plugin_modification_date
-					FamilyName FamilyItem Status vuln_publication_date NessusReportHost HostProperties preferenceName
+					FamilyName FamilyItem Status vuln_publication_date ReportHost HostProperties preferenceName
 					preferenceValues preferenceType fullName pluginId pluginName selectedValue selectedValue
 					name value preference plugin_publication_date cvss_vector patch_publication_date
 					NessusClientData_v2 Policy PluginName ServerPreferences policyComments policyName PluginItem
-					NessusReport Family Preferences PluginsPreferences FamilySelection IndividualPluginSelection PluginId
+					Report Family Preferences PluginsPreferences FamilySelection IndividualPluginSelection PluginId
 					pci-dss-compliance exploitability_ease cvss_temporal_vector exploit_framework_core cvss_temporal_score
 					exploit_available metasploit_name exploit_framework_canvas canvas_package exploit_framework_metasploit
 					plugin_type exploithub_sku exploit_framework_exploithub stig_severity plugin_name fname always_run
@@ -148,7 +148,7 @@ module Risu
 					"FamilyItem" => :end_family_item,
 					"PluginItem" => :end_plugin_item,
 					"tag" => :end_tag,
-					"NessusReportItem" => :end_report_item,
+					"ReportItem" => :end_report_item,
 					"attachment" => :end_attachment,
 					"userId" => :add_user_id,
 					"engagementId" => :add_engagement_id
@@ -276,11 +276,12 @@ module Risu
 
 				def start_report_item(_element, attributes)
 					@vals = Hash.new # have to clear this out or everything has the same references
-
+					puts "[$]#{attributes.inspect}"
 					if attributes["pluginID"] == "0"
 						@plugin = Risu::Models::Plugin.find_or_create_by(:id => 1)
 					else
 						@plugin = Risu::Models::Plugin.find_or_create_by(:id => attributes["pluginID"]) do |plugin|
+							puts "[*]#{@plugin.inspect}"
 							plugin.plugin_name = attributes["pluginName"]
 							plugin.family_name = attributes["pluginFamily"]
 						end
@@ -388,6 +389,7 @@ module Risu
 				end
 
 				def end_report_item(_)
+					puts "[&]#{@vals.inspect}"
 					@ri.update(:plugin_output => @vals["plugin_output"],
 						:plugin_name => @vals["plugin_name"],
 						:cm_compliance_info => @vals["cm:compliance-info"],
